69d01b9fb122a2028c66a5552a691bf5c421f959
dkiselev <denisich.08@yandex.ru>
2019-11-26 15:44:35 +0300

    Начал создовать объект User. Внем хочу реализовать хранение пользовательских данных таких как имя пользователя его пароль и метод проверки правильности пароля.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import ru.den.source.User;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        User newUser1  = new User("denis","Retyu");
     }
 }
