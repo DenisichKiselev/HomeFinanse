c8c9e2dc47dc4b19627e29fa073c5959bd5dd90f
dkiselev <denisich.08@yandex.ru>
2019-11-26 15:45:13 +0300

    Начал создовать объект User. Внем хочу реализовать хранение пользовательских данных таких как имя пользователя его пароль и метод проверки правильности пароля.

--- src/ru/den/source/User.java
+++ src/ru/den/source/User.java
@@ -8,21 +8,47 @@ public class User {
     int linePasswordMin = 6;
     int linePasswordMax = 10;
 
- User(String name, String passwords){
+ public User(String name, String passwords){
     this.UserName = name;
-    this.UserPassword = passwords;
+    CorrectPassword = PasswordCorrectnessCheck(passwords);
+    if (CorrectPassword){
+        this.UserPassword = passwords;
+        System.out.println("Пароль принят");
+        System.out.println("ользователь создан");
+    } else System.out.println("Вы ввели некоректный пароль");
 }
 //роверка пароля на соответствие определенным условиям
     // если они верны то переменная CorrectPassword будет истина если нет ложь
     // задача вывести пользователю  то что он ввел неправильный пароль, проверяем его и на длинну мин 6 символов максимум 10
 private boolean PasswordCorrectnessCheck(String userPassword){
      char[] ch = userPassword.toCharArray();
+
      return true;
 }
 
 private  boolean CharacterCheck(char [] ch){
-     Character[] charSimvols = new Character[] {'&','!','@', '?','#','%','{','}'};
-     return true;
+     char [] charSimvols = {'&','!','@', '?','#','%','{','}'};
+     char [] charNums = {'0','1','2', '3','4','5','6','7', '8','9'};
+     boolean a, b = false;
+     a=false;
+    for (int i = 0; i < ch.length; i++) {
+        for (int j = 0; j <charSimvols.length ; j++) {
+            if(ch[i]==charSimvols[j]){
+                b = true;
+                break;
+            }
+        }
+        for (int j = 0; j <charNums.length ; j++) {
+            if (ch[i]==charNums[j]){
+                a = true;
+                break;
+            }
+        }
+    }
+    if (a!=false & b!=false){
+        return true;
+    } else return false;
+
 }
 
 }
